3e620316fb7ad3337231f6de4f6088c2443b1a15
Nofangagah <zohrialpoison@gmail.com>
2025-06-04 08:32:09 +0700

    fix import

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import Express from 'express';
-import { editProfile } from '../controller/userController.js';
+import  editProfile  from '../controller/userController.js';
 import verifyToken from '../middleware/verifyToken.js';
 
 const router = Express.Router();
